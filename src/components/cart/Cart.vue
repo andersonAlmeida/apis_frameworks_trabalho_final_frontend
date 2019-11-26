89d59c8838ee45f43c86fbd55fcfbb026673bfd3
Anderson Almeida <andi.r.almeida@gmail.com>
2019-11-26 00:05:30 -0300

    add calc frete api

--- src/components/cart/Cart.vue
+++ src/components/cart/Cart.vue
@@ -26,11 +26,34 @@
           </tbody>
         </table>
       </div>
-
+      <!-- Subtotal -->
       <div class="col-12 d-flex justify-content-end">
         <p>
-          <strong>Total: </strong> R$ {{ getCartTotalValue }}
+          Subtotal: <strong>R$ {{ getCartTotalValue }}</strong>
         </p>
+      </div>
+      <!-- Frete -->
+      <div class="col-12">
+        <div class="row">
+          <div class="col-6">
+            <form class="form-inline">
+              <div class="input-group">
+                <div class="custom-file">
+                  <label class="label mr-2" for="frete"><strong>Frete: </strong></label>
+                  <input type="text" class="custom-input" id="frete" v-model="cep">
+                </div>
+                <div class="input-group-append">
+                  <button class="btn btn-info" type="button" @click="calcFrete">Calcular</button>
+                </div>
+            </div>
+            </form>
+          </div>
+
+          <div class="col-6 d-flex justify-content-end flex-wrap">
+            <p class="frete-info" v-if="getFrete">frete: <strong>R$ {{ frete }}</strong></p>
+            <p class="frete-info" v-if="getPrazo">{{ prazo }} dias a contar da data de débito.</p>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -38,15 +61,48 @@
 
 <script>
 import { mapGetters } from 'vuex';
+import Correios from 'node-correios';
 
 export default {
   name: 'cart',
+  data() {
+    return {
+      cep: '',
+      frete: null,
+      prazo: null,
+    };
+  },
   computed: {
-    ...mapGetters(['getCartItems', 'getCartTotalValue']),
+    ...mapGetters(['getCartItems', 'getCartTotalValue', 'getCartTotalItems']),
+    getPrazo() {
+      return this.prazo != null;
+    },
+    getFrete() {
+      return this.frete != null;
+    },
   },
   methods: {
     removeFromCart(id) {
       this.$store.dispatch('removeFromCart', id);
+    },
+    calcFrete() {
+      const correios = new Correios();
+
+      correios.calcPrecoPrazo({
+        nCdServico: '04510',
+        sCepOrigem: '90010350', // cep do senac
+        sCepDestino: this.cep,
+        nVlPeso: `${this.getCartTotalItems * 0.8}`, // peso fixo
+        nCdFormato: 1,
+        nVlComprimento: 15.5,
+        nVlAltura: 10.0,
+        nVlLargura: 20.0,
+        nVlDiametro: 15.0,
+      }).then((response) => {
+        console.log(response);
+        this.frete = response[0].Valor;
+        this.prazo = response[0].PrazoEntrega;
+      });
     },
   },
 };
@@ -57,5 +113,16 @@ export default {
   td, th {
     vertical-align: middle;
   }
+}
+.custom-input {
+  height: 100%;
+  border-radius: 3px 0 0 3px;
+  border: 1px solid var(--gray);
+  padding: 0 10px;
+}
+
+.frete-info {
+  width: 100%;
+  text-align: right;
 }
 </style>
